70fec1dd39e7dcbbf112e51b61756d842ba0b218
eglmachaieie <eglmachaieie@gmail.com>
2024-04-18 18:21:12 +0200

    upload

---
+++ src/test/java/com/gestaodevendasFeragem/Ferragem/FerragemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaodevendasFeragem.Ferragem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerragemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
